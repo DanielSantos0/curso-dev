ccbd83dd292ac6182bc129a6c67b39dd674a67c3
Daniel Santos <danielsantos.ee@gmail.com>
2025-01-24 16:23:31 -0300

    novo teste com imagem

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <img src="https://imgur.com/gallery/kbcinha-2-16-bSqMWuO" alt="kbcinhaaaa" />
+    return <h1>n tem jeito, vo te q zua <blockquote class="imgur-embed-pub" lang="en" data-id="a/bSqMWuO"  ><a href="//imgur.com/a/bSqMWuO">kbcinha 2/16</a></blockquote><script async src="//s.imgur.com/min/embed.js" charset="utf-8"></script></h1>
     
 }
 
