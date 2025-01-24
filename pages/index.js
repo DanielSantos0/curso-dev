920dcaf55f858a43ca7095848c90cdc95761dd8a
Daniel Santos <danielsantos.ee@gmail.com>
2025-01-24 16:52:01 -0300

    a

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ function Home() {
         <div>
             <h1>n tem jeito, vo te q zua</h1>
             <img 
-                src="https://i.imgur.com/EXAMPLE_IMAGE.jpg" 
+                src="https://imgur.com/OIwJ1nU.jpg" 
                 alt="Descrição da imagem" 
                 style={{ maxWidth: "100%", height: "auto" }} 
             />
