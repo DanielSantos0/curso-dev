6ef94f2cd4abf86473f5f871507b94166003fa50
Daniel Santos <danielsantos.ee@gmail.com>
2025-01-24 16:50:34 -0300

    mais um teste

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,12 @@
 function Home() {
-    const imgurEmbed = `
-        <blockquote class="imgur-embed-pub" lang="en" data-id="a/bSqMWuO">
-            <a href="//imgur.com/a/bSqMWuO">kbcinha 2/16</a>
-        </blockquote>
-        <script async src="//s.imgur.com/min/embed.js" charset="utf-8"></script>
-    `;
-
     return (
         <div>
             <h1>n tem jeito, vo te q zua</h1>
-            <div dangerouslySetInnerHTML={{ __html: imgurEmbed }} />
+            <img 
+                src="https://i.imgur.com/EXAMPLE_IMAGE.jpg" 
+                alt="Descrição da imagem" 
+                style={{ maxWidth: "100%", height: "auto" }} 
+            />
         </div>
     );
 }
