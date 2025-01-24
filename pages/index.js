7708b940aa23b508627ddfad9a7eaa7524a1bbe5
Daniel Santos <danielsantos.ee@gmail.com>
2025-01-24 16:24:47 -0300

    novo teste

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 function Home() {
-    return <h1>n tem jeito, vo te q zua <blockquote class="imgur-embed-pub" lang="en" data-id="a/bSqMWuO"  ><a href="//imgur.com/a/bSqMWuO">kbcinha 2/16</a></blockquote><script async src="//s.imgur.com/min/embed.js" charset="utf-8"></script></h1>
-    
+    return <h1>n tem jeito, vo te q zua  <img scr="https://imgur.com/OIwJ1nU" alt="kbca"></img>  </h1>
 }
 
 export default Home;
