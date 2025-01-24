bf1e2dd28d18fb4a1e12746d04f299344fcd60a0
Daniel Santos <danielsantos.ee@gmail.com>
2025-01-24 16:26:36 -0300

    novo teste

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,17 @@
 function Home() {
-    return <h1>n tem jeito, vo te q zua  <img scr="https://imgur.com/OIwJ1nU" alt="kbca"></img>  </h1>
+    const imgurEmbed = `
+        <blockquote class="imgur-embed-pub" lang="en" data-id="a/bSqMWuO">
+            <a href="//imgur.com/a/bSqMWuO">kbcinha 2/16</a>
+        </blockquote>
+        <script async src="//s.imgur.com/min/embed.js" charset="utf-8"></script>
+    `;
+
+    return (
+        <div>
+            <h1>n tem jeito, vo te q zua</h1>
+            <div dangerouslySetInnerHTML={{ __html: imgurEmbed }} />
+        </div>
+    );
 }
 
 export default Home;
